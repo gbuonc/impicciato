af5313f306fe5aacc5ccf03a80415d1dbd0ceae2
Gianluca Buoncompagni <gbuonc@gmail.com>
2016-11-02 00:41:10 +0100

    high score in game over

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,11 @@ import Game from '../views/Game';
 import GameOver from '../views/GameOver';
 
 const App = React.createClass({
+
    render(){
       return (
         <Router history={browserHistory}>
-           <Route path="/" component={Home} ></Route>
+           <Route path="/" component={Home}></Route>
            <Route path="/game/:level" component={Game} />
            <Route path="/gameover/:level/:pts" component={GameOver}></Route>
            <Route path="*" component={Home}/>
--- src/views/GameOver.js
+++ src/views/GameOver.js
@@ -2,13 +2,52 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const GameOver = React.createClass({
+  getInitialState(){
+    return {
+      highScore: JSON.parse(localStorage.getItem('highScore')) || 0,
+      highLevel: JSON.parse(localStorage.getItem('highLevel')) || 0,
+      newRecord : false
+    }
+  },
+  componentDidMount(){
+    const game = this.props.routeParams;
+    if(game.pts > this.state.highScore || game.level > this.state.highLevel){
+      if(game.pts > this.state.highScore) localStorage.setItem('highScore', JSON.stringify(game.pts));
+      if(game.level > this.state.highLevel) localStorage.setItem('highLevel', JSON.stringify(game.level));
+      this.setState({
+        highScore : game.pts > this.state.highScore ? game.pts : this.state.highScore,
+        highLevel : game.level > this.state.highLevel ? game.level : this.state.highLevel,
+        newRecord : true
+      })
+    }
+    else{
+      this.setState({
+        newRecord : false
+      })
+    }
+  },
+  renderScore(){
+    const game = this.props.routeParams;
+    if(this.state.newRecord){
+      return(
+        <div style={{marginBottom: 30}}>NUOVO RECORD! <br/>{this.state.highScore} punti - Livello {this.state.highLevel}</div>
+      )
+    }
+    return(
+      <div>
+      <div style={{fontSize:'0.8em', marginBottom: 30}}>Il tuo punteggio:<br />Livello {game.level} - {game.pts} punti</div>
+      <div style={{marginBottom: 30}}>RECORD DA BATTERE: <br/>Livello {this.state.highLevel} - {this.state.highScore} punti</div>
+      </div>
+    )
+  },
    render(){
       const game = this.props.routeParams;
       return (
          <div style={{textAlign:'center'}}>
+
             <h1>Game Over</h1>
-            <div style={{fontSize:'0.6em', marginBottom: 30}}>Livello {game.level} - {game.pts} punti</div>
-            <Link className="btn" to={'/game/1'} style={{marginBottom: 30}}>Gioca ancora</Link><br />
+            {this.renderScore()}
+            <Link className="btn" to={'/game/1'} >Gioca ancora</Link><br />
             <Link className="btn" to={'/'}>Torna alla Home</Link>
          </div>
       )
