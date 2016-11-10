4bd2a4f8bae0fc3641fd31244fb51403b87a7fb8
Gianluca Buoncompagni <gbuonc@gmail.com>
2016-11-10 01:41:28 +0100

    test to add difficulty index to dictionary words

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 import dictionary from '../dictionary/dictionary';
+import alphabet from '../helpers/alphabet';
 import GameUi from '../components/GameUi';
 import Notifications from '../components/Notifications';
 
@@ -30,6 +31,26 @@ const Game = React.createClass({
       // first get a random category
       const categories = Object.keys(dictionary);
       const category = categories[Math.floor(Math.random()*categories.length)];
+      console.log(categories);
+      // convert alphabet array to map object
+      const difficultyMap= alphabet.reduce((p,n) => {
+        p[n.char]=n.multip;
+        return p;
+      }, {});
+      console.log(difficultyMap);
+      var test = categories.map(el => dictionary[el]).map(arr => {
+        return arr.map(word =>{
+          const wordLength = word.length;
+          const wordToArr = word.toUpperCase().split('');
+          const total = wordToArr.reduce((p,n)=>{
+            return p+difficultyMap[n];
+          }, difficultyMap[wordToArr[0]]);
+          const difficulty = total/wordLength;
+          let obj = {word, wordLength, difficulty, total, difficulty};
+          return obj;
+        })
+      });
+      console.log(JSON.stringify(test));
       // remove a word from this category and set as playing word
       const randomIndex = Math.floor(Math.random()*dictionary[category].length);
       var word = dictionary[category].splice(randomIndex, 1)[0].toUpperCase();
